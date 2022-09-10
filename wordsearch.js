267e4e4db652adf71862a2aaf7a051bec9ce6208
Edlyn Shih <edlyn0418@gmail.com>
2022-09-09 20:09:04 -0400

    Refactor word search function.

--- wordsearch.js
+++ wordsearch.js
@@ -1,46 +1,26 @@
 const transpose = function (matrix) {
-    let result = [];
-    if (matrix.length === 0) {
-        return result;
-    }
-    for (let k = 0; k < matrix[0].length; k++) {
-        result.push([]);
-    }
-
-    for (let x = 0; x < matrix.length; x++) {
-        for (let y = 0; y < matrix[x].length; y++) {
-            result[y].push(matrix[x][y]);
+    const newMatrix = [];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (!newMatrix[j]){
+          newMatrix[j] = [];
         }
+        newMatrix[j].push(matrix[i][j]);
+      }
     }
-    return result;
-};
-
-// Take a matrix of letters and a word and returns true if the word is found
-
-const wordSearch = (letters, word) => { 
-     // Join all of the letters in an array (horizonal line) into a single string
-     const horizontalJoin = letters.map(ls => ls.join(''));
-
-     // Loop through each string in the array
-     for (l of horizontalJoin) {
- 
-         // If the string contains the word we're looking for, return true
-         if (l.includes(word)) {
-           return true;
-         }
-     }
- 
-     // Transpose the letters matrix
-     const transposedMatrix = transpose(letters);
- 
-     const horizontalJoin2 = transposedMatrix.map(ls => ls.join(''));
- 
-     for (l of horizontalJoin2) {
-       if (l.includes(word)) {
-         return true;
-       }
-     }
-     return false;
- }
-
-module.exports = wordSearch
+    return newMatrix;
+  };
+  
+  const wordSearch = (letters, word) => { 
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (v of verticalJoin) {
+        if (v.includes(word)) return true;
+    }
+    return false;
+  }
+  
+  module.exports = wordSearch
